ed657f9853379ecb832a9f017e979e3ebd76c8af
tGodson <tendongze95@gmail.com>
2020-10-31 14:39:26 +0100

    add the basic files to project

---
+++ webpack.common.js
@@ -0,0 +1,33 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');// eslint-disable-line import/no-extraneous-dependencies
+
+module.exports = {
+  entry: './src/index.js',
+  plugins: [new HtmlWebpackPlugin({
+    template: './src/template.html',
+  })],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[hash].[ext]',
+            outputPath: 'imgs',
+          },
+        },
+      },
+    ],
+  },
+};
