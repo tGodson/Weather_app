c5f8c67bd6d62f247a6e1da4e1f35115cf2ba9b7
tGodson <tendongze95@gmail.com>
2020-11-03 15:04:05 +0100

    configure all webpack

--- src/index.js
+++ src/index.js
@@ -1,83 +1,90 @@
-const api = {
-  key: "c3f4541f34d8e4712373b9a314c1be96",
-  base: "https://api.openweathermap.org/data/2.5/"
-}
+// import { setQuery, getResults } from "./logic";
+import './style.css';
+import Clear from './img/clear.jpg';
+import Rain from './img/rain.jpeg';
+import Cloud from './img/cloud.jpeg';
+import Mist from './img/mist.jpeg';
 
-let temp = document.querySelector('.current .temp');
-document.body.style.backgroundImage = "url('./img/cloud.jpeg')";
+const api = {
+  key: 'c3f4541f34d8e4712373b9a314c1be96',
+  base: 'https://api.openweathermap.org/data/2.5/',
+};
+
+const temp = document.querySelector('.current .temp');
+
+document.body.style.backgroundImage = `url(${Cloud})`;
 localStorage.setItem('temp', JSON.stringify(23));
 
 const searchbox = document.querySelector('.search-box');
 
-let city = document.querySelector('.location .city');
-let date = document.querySelector('.location .date');
-let weather_el = document.querySelector('.current .weather');
-let fahrenheitBtn = document.querySelector('.current .hi-low .fahrenheit');
-let celsiusBtn = document.querySelector('.current .hi-low .celsius');
-let errorMsg = document.querySelector('.error');
+const city = document.querySelector('.location .city');
+const date = document.querySelector('.location .date');
+const weatherEl = document.querySelector('.current .weather');
+const fahrenheitBtn = document.querySelector('.current .hi-low .fahrenheit');
+const celsiusBtn = document.querySelector('.current .hi-low .celsius');
+const errorMsg = document.querySelector('.error');
 
-const setQuery = (evt) => {
-  if (evt.keyCode == 13) {
-    getResults(searchbox.value);
-  }
-}
-searchbox.addEventListener('keypress', setQuery);
+const dateBuilder = (d) => {
+  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-const getResults = (query) => {
-  fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
-    .then(weather => {
-      return weather.json();
-    }).then(displayResults)
-    .catch(function(err) {
-      errorMsg.innerHTML = "Please make sure your spelling above is correct!";
-    });
-}
+  const day = days[d.getDay()];
+  const date = d.getDate();
+  const month = months[d.getMonth()];
+  const year = d.getFullYear();
+
+  return `${day} ${date} ${month} ${year}`;
+};
 
 const displayResults = (weather) => {
+  errorMsg.innerHTML = '';
   city.innerText = `${weather.name}, ${weather.sys.country}`;
 
-  let now = new Date();
+  const now = new Date();
   date.innerText = dateBuilder(now);
 
   temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
   localStorage.setItem('temp', JSON.stringify(weather.main.temp));
 
-  weather_el.innerText = weather.weather[0].main;
+  weatherEl.innerText = weather.weather[0].main;
 
-  if (weather.weather[0].main === "Clear"){
-    document.body.style.backgroundImage = "url('./img/clear.jpg')";
-  }else if(weather.weather[0].main === "Rain"){
-    document.body.style.backgroundImage = "url('./img/rain.jpeg')";
-  }else if(weather.weather[0].main === "Mist"){
-    document.body.style.backgroundImage = "url('./img/mist.jpeg')";
-  }else{
-    document.body.style.backgroundImage = "url('./img/cloud.jpeg')";
+  if (weather.weather[0].main === 'Clear') {
+    document.body.style.backgroundImage = `url(${Clear})`;
+  } else if (weather.weather[0].main === 'Rain') {
+    document.body.style.backgroundImage = `url(${Rain})`;
+  } else if (weather.weather[0].main === 'Mist') {
+    document.body.style.backgroundImage = `url(${Mist})`;
+  } else {
+    document.body.style.backgroundImage = `url(${Cloud})`;
   }
+};
 
-}
+const getResults = (query) => {
+  fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+    .then(weather => weather.json()).then(displayResults)
+    .catch(() => {
+      errorMsg.innerHTML = 'Please make sure your spelling above is correct!';
+    });
+};
 
-const dateBuilder = (d) => {
-  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-  let day = days[d.getDay()];
-  let date = d.getDate();
-  let month = months[d.getMonth()];
-  let year = d.getFullYear();
-
-  return `${day} ${date} ${month} ${year}`;
-}
-
+const setQuery = (evt) => {
+  if (evt.keyCode === 13) {
+    getResults(searchbox.value);
+  }
+};
+searchbox.addEventListener('keypress', setQuery);
 
 const fahrenheit = () => {
-  let temperature = JSON.parse(localStorage.getItem('temp'));
-  let temp_fahrenheit = (temperature * 1.8) + 32;
-  temp.innerHTML = `${Math.round(temp_fahrenheit)}<span>°F</span>`;
-}
+  const temperature = JSON.parse(localStorage.getItem('temp'));
+  const tempFahrenheit = (temperature * 1.8) + 32;
+  temp.innerHTML = `${Math.round(tempFahrenheit)}<span>°F</span>`;
+};
 fahrenheitBtn.addEventListener('click', fahrenheit);
 
 const celsius = () => {
-  let temperature = JSON.parse(localStorage.getItem('temp'));
+  const temperature = JSON.parse(localStorage.getItem('temp'));
   temp.innerHTML = `${Math.round(temperature)}<span>°C</span>`;
-}
+};
 celsiusBtn.addEventListener('click', celsius);
+
+// export { searchbox, displayResults, errorMsg };
